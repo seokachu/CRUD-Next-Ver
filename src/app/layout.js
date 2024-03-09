ef45be72f8791116977dac2e1f6320316eb4e003
박서영 <seokachuu@gmail.com>
2024-03-10 00:35:26 +0900

    Feat: a태그 Link로 변경

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import './globals.css';
 
 export const metadata = {
@@ -10,23 +11,23 @@ export default function RootLayout({ children }) {
     <html>
       <body>
         <h1>
-          <a href="/">Web</a>
+          <Link href="/">Web</Link>
         </h1>
         <ol>
           <li>
-            <a href="/read/1">html</a>
+            <Link href="/read/1">html</Link>
           </li>
           <li>
-            <a href="/read/2">css</a>
+            <Link href="/read/2">css</Link>
           </li>
         </ol>
         {children}
         <ul>
           <li>
-            <a href="/create">create</a>
+            <Link href="/create">create</Link>
           </li>
           <li>
-            <a href="/update/1">update</a>
+            <Link href="/update/1">update</Link>
           </li>
           <li>
             <input type="button" value="delete" />
